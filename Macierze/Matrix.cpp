4d390b0457ca0c0532a3eb060eb2142ff38e597d
Małgorzata Sokół <malgorzata@e-sokol.pl>
2018-04-21 23:12:32 +0200

    Próba przeciążenia <<

--- Macierze/Matrix.cpp
+++ Macierze/Matrix.cpp
@@ -11,6 +11,9 @@ Matrix<T, ROW, COL>::Matrix()
 			matrix[r][c] = 0;
 		}
 	}
+	
+	numOfRows = ROW;
+	numOfColumns = COL;
 }
 
 template<typename T, size_t ROW, size_t COL>
@@ -22,3 +25,24 @@ Matrix<T, ROW, COL>::~Matrix()
 	delete[] matrix;
 }
 
+//template<typename T, size_t ROW, size_t COL>
+//void Matrix<T, ROW, COL>::fillRow(size_t r) {
+//	
+//	for(size_t i = 0; i<COL; i++) {
+//		
+//	}
+//}
+
+
+template <typename U, size_t M, size_t N> friend ostream& operator<<(ostream& os, const Matrix<U, M, N>& m) {
+	
+	for(size_t i = 0; i<numOfRows; i++) {
+		os << '|';
+		for(size_t j = 0; j<numOfColumns; j++) {
+			os << setw(3) << m.matrix[i][j];
+		}
+		os << '|' << endl;
+	}
+	
+	return os;
+}
--- Macierze/Matrix.hpp
+++ Macierze/Matrix.hpp
@@ -14,9 +14,16 @@ public:
 	Matrix();
 	~Matrix();
 	
+//	void setValue(T value, size_t r, size_t c) {matrix[r][c] = value;}
+//	void fillRow(size_t r);
+//	void fill();
+	
+	template <typename U, size_t M, size_t N> friend ostream& operator<<(ostream& os, const Matrix<U, M, N>& m);
 	
 private:
 	T matrix[ROW][COL];
+	size_t numOfRows;
+	size_t numOfColumns;
 };
 
 #endif // MATRIX_HPP
